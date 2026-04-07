4da435976b4d1f5190bbc5e6cf40c29c11b508f9
aboalsoudsweets-png <aboalsoudsweets@gmail.com>
2026-04-07 19:05:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -735,7 +735,11 @@ function createDrinkCard(drink) {
 
 
 
-
+<div style="display: flex; justify-content: space-between; align-items: center; margin-top: 12px; border-top: 1px solid rgba(255,255,255,0.1); padding-top: 10px;">
+      
+      <div style="color: #d4af37;">
+        <strong>${drink.price}</strong> ج.م
+      </div>
 
 
 
